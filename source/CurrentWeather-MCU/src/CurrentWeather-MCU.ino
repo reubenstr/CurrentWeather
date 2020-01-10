3ee0763b689de873ba1ccf17780f804bd578e2d1
Reuben <reubenstrangelove@gmail.com>
2020-01-10 14:56:05 -0500

    Code cleanup. Added weather demo's.

--- source/CurrentWeather-MCU/src/CurrentWeather-MCU.ino
+++ source/CurrentWeather-MCU/src/CurrentWeather-MCU.ino
@@ -14,11 +14,12 @@
   Outside weather provided by the ESP01 module connecting to openweathermap.org API.
 
   A toggle on the mode ("Weather Source") switch during homing halts
-  motors after backing off the limit switches for easier position calibration.
+  motors after backing off the limit switches for easier position calibration, 
+  and cycles through a weather effects demo.
 
 */
 
-#define DEBUG_OUTPUT 1
+#define DEBUG_OUTPUT 0
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -39,6 +40,7 @@
 #define PIN_DRIVER_2_DIR 4
 #define PIN_DRIVER_3_STEP 7
 #define PIN_DRIVER_3_DIR 6
+#define PIN_DRIVER_ENABLE_ALL A7
 #define PIN_LIMIT_TEMPERATURE A3
 #define PIN_LIMIT_PRESSURE A2
 #define PIN_LIMIT_HUMIDITY A1
@@ -50,6 +52,7 @@ long const STEPPER_ACCELERATION = 2500; // Instantious full speed.
 long const STEPPER_MAX_SPEED = 20000;
 long const STEPPER_SPEED = 4000;
 float const STEPPER_STEPS_PER_INCH = 1128.888;
+
 
 float const STEPPER_INCHES_PER_TEMPERATURE_UNIT = 0.107; // Distance between ticks on the gauge display
 float const STEPPER_INCHES_PER_PRESSURE_UNIT = 0.130;    // Distance between ticks on the gauge display
@@ -92,12 +95,19 @@ struct Weather
   unsigned int sum;
 };
 
-Weather weather;
-unsigned int const insideWeatherDataUpdateRate = 25; // ms, effects stepper smoothness.
+enum PrecipitationType
+{
+  Rain,
+  Snow,
+  RainSnowMix
+};
 
-float const temperatureInsideCalibrationOffset = -6.0;
-float const pressureInsideCalibrationOffset = 0;
-float const humidityInsideCalibrationOffset = 7.0;
+Weather weather;
+unsigned int const weatherDataUpdateRate = 250; // ms, effects stepper smoothness.
+
+float const temperatureInsideCalibrationOffset = -4.0;  // Degrees F
+float const pressureInsideCalibrationOffset = 0;        // hPA
+float const humidityInsideCalibrationOffset = 7.0;      // Percent
 
 AccelStepper stepperTemperature(AccelStepper::DRIVER, PIN_DRIVER_3_STEP, PIN_DRIVER_3_DIR);
 AccelStepper stepperPressure(AccelStepper::DRIVER, PIN_DRIVER_2_STEP, PIN_DRIVER_2_DIR);
@@ -125,10 +135,12 @@ void setup()
   pinMode(PIN_LIMIT_PRESSURE, INPUT);
   pinMode(PIN_LIMIT_HUMIDITY, INPUT);
   pinMode(PIN_SWITCH_MODE, INPUT);
+  pinMode(PIN_DRIVER_ENABLE_ALL, OUTPUT);
   digitalWrite(PIN_LIMIT_TEMPERATURE, HIGH); // turn on pullup resistors
   digitalWrite(PIN_LIMIT_PRESSURE, HIGH);    // turn on pullup resistors
   digitalWrite(PIN_LIMIT_HUMIDITY, HIGH);    // turn on pullup resistors
-  digitalWrite(PIN_SWITCH_MODE, HIGH);       // turn on pullup resistors
+  digitalWrite(PIN_SWITCH_MODE, HIGH);       // turn on pullup resistors 
+  digitalWrite(PIN_DRIVER_ENABLE_ALL, LOW); // Enable all stepper motor drivers
 
   // Initialize stepper motors.
   stepperTemperature.setAcceleration(STEPPER_ACCELERATION);
@@ -140,6 +152,11 @@ void setup()
   stepperTemperature.setSpeed(STEPPER_SPEED);
   stepperPressure.setSpeed(STEPPER_SPEED);
   stepperHumidity.setSpeed(STEPPER_SPEED);
+
+  stepperTemperature.setPinsInverted(true, false, false);
+  stepperPressure.setPinsInverted(true, false, false);
+  stepperHumidity.setPinsInverted(true, false, false);
+
 
   // Initialize WS2812b LED strips.
   stripTemperature.setBrightness(LED_POINTER_BRIGHTNESS);
@@ -182,15 +199,20 @@ void loop()
   UpdatePointerColors();
   CheckLimitSwitches();
   UpdateStepperMotors();
-  UpdateCloudCoverAndPrecipitationLEDs();
+  UpdateCloudCoverAndPrecipitationLEDs(weather.weatherId);
 }
 
 // Step motors (if needed) (does not use acceleration).
 void UpdateStepperMotors()
 {
-  stepperTemperature.run();
-  stepperPressure.run();
-  stepperHumidity.run();
+
+   stepperTemperature.setSpeed(STEPPER_SPEED);
+  stepperPressure.setSpeed(STEPPER_SPEED);
+  stepperHumidity.setSpeed(STEPPER_SPEED);
+
+  stepperTemperature.runSpeedToPosition();
+  stepperPressure.runSpeedToPosition();
+  stepperHumidity.runSpeedToPosition();
 }
 
 // Update LEDs and steppers with weather information.
@@ -201,14 +223,16 @@ void UpdatePointerPositions()
   float pressureData;
   float humidityData;
 
+   // Add delay due to execution overhead to prevent stepper motor slow down.   
+   if (millis() < oldMillis + weatherDataUpdateRate)
+    {      
+      return;       
+    }
+    oldMillis = millis();
+
   // Select weather data source: inside or outside (sensor or ESP via UART)
   if (mode == MODE_INSIDE)
   {
-    // Add delay due to execution overhead to prevent stepper motor slow down.
-    if (millis() > oldMillis + insideWeatherDataUpdateRate)
-    {
-      oldMillis = millis();
-
       // Acquire data from BME280 sensor (per Adafruit example sketch).
       sensors_event_t temp_event, pressure_event, humidity_event;
       bme_temp->getEvent(&temp_event);
@@ -218,22 +242,22 @@ void UpdatePointerPositions()
       temperatureData = round((temp_event.temperature * 9.0 / 5.0) + 32.0 + temperatureInsideCalibrationOffset); // Convert C to F
       pressureData = round(pressure_event.pressure + pressureInsideCalibrationOffset);
       humidityData = round(humidity_event.relative_humidity + humidityInsideCalibrationOffset);
-    }
+    
   }
   else if (mode == MODE_OUTSIDE)
   {
     // Data from ESP (via UART).
     temperatureData = weather.temperature;
     pressureData = weather.pressure;
-    humidityData = weather.humidity;
-
-    temperatureData = 10; // TEMP
-    pressureData = 960;   // TEMP
-    humidityData = 10;    // TEMP
+    humidityData = weather.humidity;    
   }
 
   // Convert temperature to position (add 10 degrees offset to scale temperature out of negative values).
   float positionTemperature = (temperatureData + 10) * STEPPER_STEPS_PER_INCH * STEPPER_INCHES_PER_TEMPERATURE_UNIT;
+   if (positionTemperature < 0)
+  {
+    positionTemperature = 0;
+  }
   if (positionTemperature > STEPPER_MAX_POSITION)
   {
     positionTemperature = STEPPER_MAX_POSITION;
@@ -242,6 +266,10 @@ void UpdatePointerPositions()
 
   // Convert pressure to position (subtrace 950 units to scale into gauge and stepper space).
   float positionPressure = (pressureData - 950) * STEPPER_STEPS_PER_INCH * STEPPER_INCHES_PER_PRESSURE_UNIT;
+   if (positionPressure < 0)
+  {
+    positionPressure = 0;
+  }
   if (positionPressure > STEPPER_MAX_POSITION)
   {
     positionPressure = STEPPER_MAX_POSITION;
@@ -250,6 +278,10 @@ void UpdatePointerPositions()
 
   // Convert humidity to position.
   float positionHumdity = (humidityData)*STEPPER_STEPS_PER_INCH * STEPPER_INCHES_PER_HUMITIY_UNIT;
+   if (positionHumdity < 0)
+  {
+    positionHumdity = 0;
+  }
   if (positionHumdity > STEPPER_MAX_POSITION)
   {
     positionHumdity = STEPPER_MAX_POSITION;
@@ -376,17 +408,14 @@ void CheckLimitSwitches()
 }
 
 // weatherId codes provided by: https://openweathermap.org/weather-conditions
-void UpdateCloudCoverAndPrecipitationLEDs()
-{ 
-  weather.weatherId = 700;  // TEMP
-
+void UpdateCloudCoverAndPrecipitationLEDs(unsigned int weatherId)
+{
   static unsigned long oldMillis;
   static unsigned long triggerMillis;
   static unsigned long lightningMillis;
   const byte maxLEDs = 13;
   static uint32_t leds[maxLEDs];
   static uint32_t ledsBackup[maxLEDs];
-  bool precipitationFlag;
 
   unsigned long triggerDelay;
   byte triggeredLeds;
@@ -395,109 +424,132 @@ void UpdateCloudCoverAndPrecipitationLEDs()
   static bool lightningFlashFlag = false;
 
   // Do not use up too much CPU time processing LEDs.
-  if (millis() < oldMillis + 10)
+  if (millis() < oldMillis + 25)
   {
     return;
   }
 
   oldMillis = millis();
 
-  // TODO: 511 611 612 613 615 616
-  // TODO: most 700's
-
   // Configure precipitation intensity (rain, drizzle, and snow) from weatherId.
-  // Go faster, trigger more leds as intensity increases.
-  switch (weather.weatherId)
+  // Blink faster and trigger more leds as intensity increases.
+  switch (weatherId)
   {
-  case 200:
-  case 210:
-  case 230:
-  case 300:
-  case 310:
-  case 500:
-  case 520:
-  case 600:
-  case 620:
+  case 200: // thunderstorm with light rain
+  case 210: // light thunderstorm
+  case 230: // thunderstorm with light drizzle
+  case 300: // light intensity drizzle
+  case 310: // light intensity drizzle rain
+  case 500: // light rain
+  case 520: // light intensity shower rain
+  case 600: // light snow
+  case 612: // light shower sleet
+  case 615: // light rain and snow
+  case 620: // light shower snow
     triggerDelay = 800;
     triggeredLeds = 1;
     lightningIntensity = 1;
     break;
 
-  case 201:
-  case 211:
-  case 221:
-  case 231:
-  case 301:
-  case 311:
-  case 501:
-  case 521:
-  case 601:
+  case 201: // thunderstorm with rain
+  case 211: // thunderstorm
+  case 221: // ragged thunderstorm
+  case 231: // thunderstorm with drizzle
+  case 301: // drizzle
+  case 311: // drizzle rain
+  case 501: // moderate rain
+  case 521: // shower rain
+  case 601: // snow
+  case 603: // shower sleet
+  case 616: // rain and snow
     triggerDelay = 650;
     triggeredLeds = 2;
     lightningIntensity = 2;
     break;
 
-  case 202:
-  case 212:
-  case 232:
-  case 302:
-  case 312:
-  case 313:
-  case 502:
-  case 522:
-  case 531:
-  case 602:
-  case 621:
+  case 202: // thunderstorm with heavy rain
+  case 212: // heavy thunderstorm
+  case 232: // thunderstorm with heavy drizzle
+  case 302: // heavy intensity drizzle
+  case 312: // heavy intensity drizzle rain
+  case 313: // shower rain and drizzle
+  case 502: // heavy intensity rain
+  case 511: // freezing rain
+  case 522: // heavy intensity shower rain
+  case 531: // ragged shower rain
+  case 602: // heavy snow
+  case 611: // sleet
+  case 621: // shower snow
     triggerDelay = 400;
     triggeredLeds = 3;
     lightningIntensity = 3;
     break;
 
-  case 314:
-  case 321:
-  case 503:
+  case 314: // heavy shower rain and drizzle
+  case 321: // shower drizzle
+  case 503: // very heavy rain
+  case 622: // heavy snow shower
     triggerDelay = 250;
     triggeredLeds = 4;
     break;
 
-  case 504:
-  case 622:
+  case 504: // extreme rain
     triggerDelay = 100;
     triggeredLeds = 5;
     break;
 
+  case 701: // mist
+  case 711: // smoke
+  case 721: // haze
+  case 731: // sand/dust whirls
+  case 741: // fog
+  case 751: // sand
+  case 761: // dust
+  case 762: // volcanic ash
+  case 771: // squalls
+    triggerDelay = 750;
+    break;
+
+  case 781:
+    triggerDelay = 350;
+    break;
+
   default:
     // Should not occur.
-    triggerDelay = 20000;
+    triggerDelay = 750;
   }
-
-  // Special case where there is weather effects but not precipitation (Thunderstorms).
-  if (weather.weatherId == 210 || weather.weatherId == 211 || weather.weatherId == 212)
-    precipitationFlag = false;
-  else
-    precipitationFlag = true;
 
   if (mode == MODE_INSIDE)
   {
-    for (int i = 9; i < 3 + 21; i++)
+    for (int i = 9; i < 21; i++)
     {
       stripText.setPixelColor(i, stripText.Color(0, 0, 0));
     }
   }
   else if (mode == MODE_OUTSIDE)
   {
-    // Thunderstorm (with rain or without rain).
-    if (weather.weatherId >= 200 && weather.weatherId <= 299)
+    if (millis() > triggerMillis + triggerDelay)
     {
-      if (millis() > triggerMillis + triggerDelay)
+      triggerMillis = millis();
+
+      // Thunderstorm (with rain or without rain).
+      if (weatherId >= 200 && weatherId <= 299)
       {
-        triggerMillis = millis();
+        bool precipitationFlag;
+        if (weatherId == 210 || // light thunderstorm
+            weatherId == 211 || // thunderstorm
+            weatherId == 212 || // heavy thunderstorm
+            weatherId == 221)   // ragged thunderstorm
+          precipitationFlag = false;
+        else
+          precipitationFlag = true;
 
         if (millis() > lightningMillis)
         {
           triggerMillis = millis() - triggerDelay + triggerDelay / 10; // Reduce delay for a lightning pop.
           lightningMillis = millis() + random(1500 / (lightningIntensity * 2), 2500 - (lightningIntensity * 250));
           lightningFlashFlag = true;
+
           byte lightningLedsCount;
           if (lightningIntensity == 1)
             lightningLedsCount = 1;
@@ -505,6 +557,7 @@ void UpdateCloudCoverAndPrecipitationLEDs()
             lightningLedsCount = 4;
           if (lightningIntensity == 3)
             lightningLedsCount = maxLEDs;
+
           for (int j = 0; j < lightningLedsCount; j++)
           {
             leds[random(0, maxLEDs)] = Color(94, 165, 94);
@@ -533,35 +586,33 @@ void UpdateCloudCoverAndPrecipitationLEDs()
             }
           }
           if (precipitationFlag)
-            Precipitation(leds, maxLEDs, triggeredLeds, Color(0, 0, 255));
+          {
+            Precipitation(leds, maxLEDs, triggeredLeds, Rain);
+          }
         }
       }
-    }
-    // Rainy
-    else if (weather.weatherId >= 500 && weather.weatherId <= 599)
-    {
-      if (millis() > triggerMillis + triggerDelay)
+      // Rainy
+      else if (weatherId >= 500 && weatherId <= 599)
       {
-        triggerMillis = millis();
-        Precipitation(leds, maxLEDs, triggeredLeds, Color(0, 0, 255));
+        Precipitation(leds, maxLEDs, triggeredLeds, Rain);
       }
-    }
-    // Snowy
-    else if (weather.weatherId >= 600 && weather.weatherId <= 699)
-    {
-      if (millis() > triggerMillis + triggerDelay)
+      // Snowy (and rain/snow mixes).
+      else if (weatherId >= 600 && weatherId <= 699)
       {
-        triggerMillis = millis();
-        Precipitation(leds, maxLEDs, triggeredLeds, Color(94, 165, 94));
+        // IDs 611, 612, 613, 615, 616 are rain and snow mixes (and sleet).
+        if (weatherId >= 611 && weatherId <= 616)
+        {
+          Precipitation(leds, maxLEDs, triggeredLeds, RainSnowMix);
+        }
+        else
+        {
+          Precipitation(leds, maxLEDs, triggeredLeds, Snow);
+        }
       }
-    }
-    // Mist, Fog, Smoke, Sand, Dust, ect.
-    else if (weather.weatherId >= 700 && weather.weatherId <= 775)
-    {
-      static bool toggle;
-      if (millis() > triggerMillis + 750)
+      // Mist, Fog, Smoke, Sand, Dust, ect.
+      else if (weatherId >= 700 && weatherId <= 775)
       {
-        triggerMillis = millis();
+        static bool toggle;
         for (int i = 0; i < maxLEDs; i++)
         {
           if (toggle)
@@ -575,23 +626,10 @@ void UpdateCloudCoverAndPrecipitationLEDs()
           toggle = !toggle;
         }
       }
-    }
-    // Tornado
-    else if (weather.weatherId == 781)
-    {
-      static bool toggle;
-      if (millis() > triggerMillis + 350)
+      // Tornado
+      else if (weatherId == 781)
       {
-        triggerMillis = millis();
-
-        if (toggle)
-        {
-          toggle = false;
-        }
-        else
-        {
-          toggle = true;
-        }
+        static bool toggle;
         for (int i = 0; i < maxLEDs; i++)
         {
           if (toggle)
@@ -603,48 +641,49 @@ void UpdateCloudCoverAndPrecipitationLEDs()
             leds[i] = Color(0, 0, 0);
           }
         }
+        toggle = !toggle;
       }
-    }
-    // Clear/Cloudy
-    else if (weather.weatherId >= 800 && weather.weatherId <= 804)
-    {
-      // Cloud cover: 800 = 0%-10%, 801 = 11%-25%, 802 = 25%-50%, 803 = 51%-84%, 804 = 85%-100%
-      byte switchOver;
-      if (weather.weatherId == 800)
-        switchOver = maxLEDs;
-      if (weather.weatherId == 801)
-        switchOver = 1;
-      if (weather.weatherId == 802)
-        switchOver = 3;
-      if (weather.weatherId == 803)
-        switchOver = 7;
-      if (weather.weatherId == 804)
-        switchOver = 11;
-
-      for (int i = 0; i < maxLEDs; i++)
+      // Clear/Cloudy
+      else if (weatherId >= 800 && weatherId <= 804)
       {
-        if (i >= switchOver)
+        // Cloud cover: 800 = 0%-10%, 801 = 11%-25%, 802 = 25%-50%, 803 = 51%-84%, 804 = 85%-100%
+        byte switchOver;
+        if (weatherId == 800)
+          switchOver = maxLEDs;
+        if (weatherId == 801)
+          switchOver = 1;
+        if (weatherId == 802)
+          switchOver = 3;
+        if (weatherId == 803)
+          switchOver = 7;
+        if (weatherId == 804)
+          switchOver = 11;
+
+        for (int i = 0; i < maxLEDs; i++)
         {
-          leds[i] = Color(127, 127, 0); // Sun
-        }
-        else
-        {
-          leds[i] = Color(94, 165, 94); // Clouds
+          if (i >= switchOver)
+          {
+            leds[i] = Color(127, 127, 0); // Sun
+          }
+          else
+          {
+            leds[i] = Color(94, 165, 94); // Clouds
+          }
         }
       }
-    }
 
-    // Copy leds data into neopixel strip.
-    for (int i = 9; i < 21; i++)
-    {
-      stripText.setPixelColor(i, leds[i - 9]);
+      // Copy leds data into neopixel strip.
+      for (int i = 9; i < 21; i++)
+      {
+        stripText.setPixelColor(i, leds[i - 9]);
+      }
     }
   }
 
   stripText.show();
 }
 
-void Precipitation(uint32_t leds[], byte maxLEDs, byte triggeredLeds, uint32_t color)
+void Precipitation(uint32_t leds[], byte maxLEDs, byte triggeredLeds, PrecipitationType type)
 {
   for (int j = 0; j < maxLEDs; j++)
   {
@@ -652,7 +691,60 @@ void Precipitation(uint32_t leds[], byte maxLEDs, byte triggeredLeds, uint32_t c
   }
   for (int j = 0; j < triggeredLeds; j++)
   {
-    leds[random(0, maxLEDs)] = color;
+    if (type == Rain)
+    {
+      leds[random(0, maxLEDs)] = Color(0, 0, 255);
+    }
+    else if (type == Snow)
+    {
+      leds[random(0, maxLEDs)] = Color(94, 165, 94);
+    }
+    else if (type == RainSnowMix)
+    {
+      if (random(0, 2))
+      {
+        leds[random(0, maxLEDs)] = Color(0, 0, 255);
+      }
+      else
+      {
+        leds[random(0, maxLEDs)] = Color(94, 165, 94);
+      }
+    }
+  }
+}
+
+// Cycle through weather effects for demonstration and calibration.
+void DemoCloudCoverAndPrecipitation()
+{
+  static unsigned long oldMillis = millis();
+  byte state = 0;
+
+  while (state != 9)
+  {
+    if (millis() > oldMillis + 5000)
+    {
+      oldMillis = millis();
+      state++;
+    }
+
+    if (state == 0)
+      UpdateCloudCoverAndPrecipitationLEDs(802); // scattered clounds
+    if (state == 1)
+      UpdateCloudCoverAndPrecipitationLEDs(804); // overcast
+    if (state == 2)
+      UpdateCloudCoverAndPrecipitationLEDs(500); // light rain
+    if (state == 3)
+      UpdateCloudCoverAndPrecipitationLEDs(503); // very heavy rain
+    if (state == 4)
+      UpdateCloudCoverAndPrecipitationLEDs(201); // thunderstorm with rain
+    if (state == 5)
+      UpdateCloudCoverAndPrecipitationLEDs(616); // rain and snow
+    if (state == 6)
+      UpdateCloudCoverAndPrecipitationLEDs(601); // snow
+    if (state == 7)
+      UpdateCloudCoverAndPrecipitationLEDs(781); // tornado
+    if (state == 8)
+      UpdateCloudCoverAndPrecipitationLEDs(741); // fog
   }
 }
 
@@ -733,6 +825,8 @@ void HomeSteppers()
 
   if (haltForCalibrationFlag)
   {
+    DemoCloudCoverAndPrecipitation();
+
     while (1)
       ; // Halt program for home position calibration.
   }
@@ -875,8 +969,6 @@ void SetGaugeColors()
   for (int i = 0; i < LED_NUM_LEDS_PER_GAUGE; i++)
   {
     int mappedPositionToColorWheel = map(i, 0, LED_NUM_LEDS_PER_GAUGE, 170, 0);
-    stripTemperature.setPixelColor(0, Wheel(mappedPositionToColorWheel));
-
     stripGauges.setPixelColor(i + 0, Wheel(mappedPositionToColorWheel));
     stripGauges.setPixelColor(40 - i, Wheel(mappedPositionToColorWheel));
     stripGauges.setPixelColor(i + 40, Wheel(mappedPositionToColorWheel));
